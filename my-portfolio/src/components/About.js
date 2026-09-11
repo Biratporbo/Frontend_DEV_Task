81bee2f393fb24d207777875439894afdd834912
Birat <biratdey200311@gmail.com>
2026-09-11 21:27:12 +0530

    Add About component for personal introduction

---
+++ my-portfolio/src/components/About.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <section id="about">
+      <h2>About Me</h2>
+      <p>
+        Hello, Myself Birat! I am a passionate web developer learning React. I love building
+        interactive, responsive, and user-friendly web applications. My goal is to 
+        turn complex problems into simple, beautiful, and intuitive interfaces.
+      </p>
+    </section>
+  );
+};
+
+export default About;

e04ce228354d5922e49817abc366a117f2d9c788
Birat <biratdey200311@gmail.com>
2026-09-11 21:27:32 +0530

    Add Education component for displaying educational background

---
+++ my-portfolio/src/components/Education.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Education = () => {
+  return (
+    <section id="education">
+      <h2>Education</h2>
+      <div>
+        <h3>Bachelor of Computer Application</h3>
+        <p>Techno India University, West Bengal | 2023 - Present</p>
+        <p>Relevant Coursework: Web Development, Data Structures, Software Engineering.</p>
+      </div>
+    </section>
+  );
+};
+
+export default Education;

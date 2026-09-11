61ec1b9e8d186867607c17d8b134ddfc61709034
Birat <biratdey200311@gmail.com>
2026-09-11 21:27:48 +0530

    Add Header component for navigation and title display

---
+++ my-portfolio/src/components/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header>
+      <nav>
+        <h1>My Portfolio</h1>
+        <ul>
+          <li><a href="#about">About Me</a></li>
+          <li><a href="#education">Education</a></li>
+          <li><a href="#skills">Skills</a></li>
+          <li><a href="#contact">Contact</a></li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;

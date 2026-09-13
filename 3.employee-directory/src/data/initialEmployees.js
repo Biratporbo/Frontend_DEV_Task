08505d037301bfef04823000f3542b966bd87cd1
Birat <biratdey200311@gmail.com>
2026-09-13 15:27:09 +0530

    Add initial mock employee data for GreenField Farm

---
+++ 3.employee-directory/src/data/initialEmployees.js
@@ -0,0 +1,78 @@
+// Initial mock employee data for GreenField Farm
+const initialEmployees = [
+  {
+    id: "EMP-101",
+    name: "Ramesh Sharma",
+    department: "Crop Production",
+    gender: "Male",
+    phone: "9876543210",
+    localAddress: "Staff Quarter 4, GreenField Farm Campus, Karnal",
+    permanentAddress: "Village Pehowa, Post Office Kurukshetra, Haryana - 136128"
+  },
+  {
+    id: "EMP-102",
+    name: "Sunita Verma",
+    department: "Dairy & Livestock",
+    gender: "Female",
+    phone: "9812345678",
+    localAddress: "Flat 2B, Kisan Colony, Sector 12, Karnal",
+    permanentAddress: "House 45, Near Milk Plant, Rohtak, Haryana - 124001"
+  },
+  {
+    id: "EMP-103",
+    name: "Kuldeep Singh",
+    department: "Farm Machinery",
+    gender: "Male",
+    phone: "9723456789",
+    localAddress: "Near Tractor Workshop, Farm Road 2, Karnal",
+    permanentAddress: "VPO Bhikhiwind, District Tarn Taran, Punjab - 143303"
+  },
+  {
+    id: "EMP-104",
+    name: "Pooja Patel",
+    department: "Horticulture",
+    gender: "Female",
+    phone: "9654321876",
+    localAddress: "Green Valley Hostel, Room 14, Karnal",
+    permanentAddress: "Plot 89, Anand Nagar, Anand, Gujarat - 388001"
+  },
+  {
+    id: "EMP-105",
+    name: "Amit Kumar",
+    department: "Harvesting & Storage",
+    gender: "Male",
+    phone: "9543216789",
+    localAddress: "Grain Silo Quarters, Block A, Karnal",
+    permanentAddress: "Village Rasulpur, Tehsil Kaithal, Haryana - 136027"
+  },
+  {
+    id: "EMP-106",
+    name: "Neelam Devi",
+    department: "Administration",
+    gender: "Female",
+    phone: "9432167890",
+    localAddress: "House 102, Model Town, Karnal",
+    permanentAddress: "House 102, Model Town, Karnal, Haryana - 132001"
+  },
+  {
+    id: "EMP-107",
+    name: "Gurpreet Dhillon",
+    department: "Crop Production",
+    gender: "Male",
+    phone: "9321456789",
+    localAddress: "Staff Quarter 7, GreenField Farm Campus, Karnal",
+    permanentAddress: "VPO Kotkapura, District Faridkot, Punjab - 151204"
+  },
+  {
+    id: "EMP-108",
+    name: "Priya Nair",
+    department: "Dairy & Livestock",
+    gender: "Female",
+    phone: "9123456780",
+    localAddress: "Flat 104, Sunrise Apartments, Karnal",
+    permanentAddress: "Palakkad Road, Alathur Post, Palakkad, Kerala - 678541"
+  }
+];
+
+export default initialEmployees;
+
